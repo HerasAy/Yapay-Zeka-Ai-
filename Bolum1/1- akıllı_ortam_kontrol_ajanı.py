4bc469545d073ed962eb087fd79f32050e2bc31f
Ayşegül Deniz <98961705+HerasAy@users.noreply.github.com>
2026-01-28 22:56:28 +0300

    Delete Bolum1 directory

--- Bolum1/1- akıllı_ortam_kontrol_ajanı.py
+++
@@ -1,8 +0,0 @@
-import random
-import time
-import pandas as pd
-
-# Sabit Değişkenler (Environment Constants)
-TARGET_TEMP = 22  # Hedef Sıcaklık
-TEMP_TOLERANCE = 2 # Tolerans Aralığı (+/- 2 derece)
-TARGET_HUMIDITY = 45 # Hedef Nem
